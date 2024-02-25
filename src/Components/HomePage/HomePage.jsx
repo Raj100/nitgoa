1f83add7f5bf54c9efb3dd56e9ceac2c56ca109f
Shivansh-22866 <shivanshp0418@gmail.com>
2024-02-26 03:08:08 +0530

    a

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -15,6 +15,17 @@ const HomePage = () => {
     { title: "Oct 6, 2024", content: "All nine eligible B.Tech. programmes have been accredited by the NBA" },
   ];
 
+  const quickLinks = [
+    { title: "Oct 7, 2024", content: "All nine eligible B.Tech. programmes have been accredited by the NBA" },
+    { title: "Oct 7, 2024", content: "All nine eligible B.Tech. programmes have been accredited by the NBA" },
+    { title: "Oct 7, 2024", content: "All nine eligible B.Tech. programmes have been accredited by the NBA" },
+    { title: "Oct 7, 2024", content: "All nine eligible B.Tech. programmes have been accredited by the NBA" },
+    { title: "Oct 7, 2024", content: "All nine eligible B.Tech. programmes have been accredited by the NBA" },
+    { title: "Oct 7, 2024", content: "All nine eligible B.Tech. programmes have been accredited by the NBA" },
+    { title: "Oct 7, 2024", content: "All nine eligible B.Tech. programmes have been accredited by the NBA" },
+    { title: "Oct 7, 2024", content: "All nine eligible B.Tech. programmes have been accredited by the NBA" },
+  ]
+
   return (
     <>
     <HomePageCarousel/>
@@ -26,9 +37,9 @@ const HomePage = () => {
       contentItems={newsContent}
       svgPath="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z"
     />
-    <HomePage 
+    <HomePageCards
       title="Quick Links"
-      contentItems={newsContent}
+      contentItems={quickLinks}
       svgPath="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"
     />
     </>
--- src/Components/LogoNavbar.jsx
+++ src/Components/LogoNavbar.jsx
@@ -3,7 +3,7 @@ import Logo from '../assets/nitlogo.png';
 const LogoNavbar = () => {
   return (
     <>
-    <header className='flex flex-row justify-between items-center w-screen p-4 sticky top-0'>
+    <header className='flex flex-row justify-between items-center w-full p-4 sticky top-0 bg-white z-50'>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class='w-8 h-8'>
             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
         </svg>
