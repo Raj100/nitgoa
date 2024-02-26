6b23e199df5890c6b853c08787e6e90c29517d66
Raj Rawariya <98628762+Raj100@users.noreply.github.com>
2024-02-26 16:41:57 +0530

    commit #1 Testing colors

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
-
+import logo from "../assets/logo.png"
+import menu from "../assets/menu.svg"
 const Navbar = () => {
   return (
     <>
+    <div className=' h-12 px-4 py-3 flex justify-between bg-defaultth text-white text-xs'>
+      <p>English</p>
+      <div className="">
+        <a href="" className=' pr-[10px] '>G.I.A.N</a>
+        <a href="">Login</a>
+      </div>
+    </div>
+
     <header>
-        <h1 className='bg-blue-500'>header</h1>
+
+      <div className='h-[70px] bg-defaultbg'>
+        <img src={menu} alt="" />
+        <img src={logo}></img>
+      </div>
+
     </header>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'defaultth': '#4361EE',
+        'defaultbg': '#F7F7F7'
+      }
+      
+    },
   },
   plugins: [],
 }
